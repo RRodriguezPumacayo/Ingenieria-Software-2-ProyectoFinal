81889cfae30abcb1e38b628d48ce12cabbdda1ed
AntonyAroni <aaronij@unsa.edu.pe>
2023-12-18 16:06:43 -0500

    Solucionar code smells y agregar prueba unitaria.

--- utilities/Authentication.js
+++ utilities/Authentication.js
@@ -1,20 +1,19 @@
-var basicAuth = require('basic-auth');
-var db = require('./SQL');
-var bcrypt = require('bcrypt-nodejs');
+let basicAuth = require('basic-auth');
+const db = require('./SQL');
+let bcrypt = require('bcrypt-nodejs');
 
 exports.BasicAuthentication = function(request, response, next) {
  
     function unauthorized(response) {
         response.set('WWW-Authenticate', 'Basic realm=Protected Area');
         return response.send(401);
-    };
- 
-    var user = basicAuth(request);
+    }
 
-    if (!user || !user.name || !user.pass) {
+    let user = basicAuth(request);
+
+    if (!user?.name || !user?.pass) {
         return unauthorized(response);
-    };
-    
+    }
     db.query('SELECT * FROM `users` WHERE `username` = ?', [user.name], function (error, results, fields) {
         if(results.length > 0 && bcrypt.compareSync(user.pass, results[0]['password'])) {
             response.pageInfo.user = {
--- utilities/SQL.js
+++ utilities/SQL.js
@@ -3,11 +3,10 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '123456',
     database: 'recipebook',
     multipleStatements: true  
 });
-
 connection.connect(function(err) {
     if (err) {
         console.error('Error: Could not connect to MySQL...\r\n');

a35f74258c65a53d85ab8476ba4a0209354e6b70
AntonyAroni <aaronij@unsa.edu.pe>
2023-12-18 20:41:12 -0500

    Solucionar code smells

--- react/src/components/pages/HomePage.js
+++ react/src/components/pages/HomePage.js
@@ -1,16 +1,12 @@
-import {react} from 'react';
 import {Link} from 'react-router';
 
 export default class HomePage extends React.Component {
-    constructor(){
-        super(...arguments);
-    }
     render(){
         return(        
             <div>
                 <p>Welcome to the Recipe Book Application...</p>
 
-                <h4><Link to="/recipes">View My Recipes</Link></h4>        
+                <h4><Link to="/recipes" onlyActiveOnIndex>View My Recipes</Link></h4>
             </div>
         );
     }
--- react/src/utilities/Helper.js
+++ react/src/utilities/Helper.js
@@ -1,75 +1,51 @@
 export class Helper {
- 
-    constructor(){ 
-
-    }    
-    
     static log(msg) {
         console.log(msg);
-    }     
-    
-    static RandomString(strLength){
-        if(typeof strLength === 'undefined') { 
-            strLength = 8;
-        }
+    }
 
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    static RandomString(strLength = 8){
+        let text = "";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        for(var i=0; i < strLength; i++) {
+        for(let i = 0; i < strLength; i++) {
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         }
         return text;
-
     }
 
-    static CreateCookie(name,value,days){
+    static CreateCookie(name, value, days){
+        let expires = "";
         if (days) {
-            var date = new Date();
-            date.setTime(date.getTime()+(days*24*60*60*1000));
-            var expires = "; expires="+date.toGMTString();
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
         }
-        else {
-            var expires = "";
-        }
-        document.cookie = name+"="+value+expires+"; path=/";        
+        document.cookie = `${name}=${value}${expires}; path=/`;
     }
 
     static ReadCookie(name){
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        const nameEQ = `${name}=`;
+        const cookies = document.cookie.split(';');
+        for(let cookie of cookies) {
+            let trimmedCookie = cookie;
+            while (trimmedCookie.startsWith(' ')) trimmedCookie = trimmedCookie.substring(1, trimmedCookie.length);
+            if (trimmedCookie.startsWith(nameEQ)) return trimmedCookie.substring(nameEQ.length, trimmedCookie.length);
         }
         return null;
     }
 
     static DestroyCookie(name) {
-        this.CreateCookie(name,"",-1);
+        this.CreateCookie(name, "", -1);
     }
-    
+
     static StripTrailingSlash(str){
-
-        // string is probably single '/', no need to strip last '/'
-        if(str.length === 1) {
-            return str;
+        if(str.length > 1 && str.charAt(str.length - 1) === "/") {
+            str = str.substr(0, str.length - 1);
         }
-        else {
-            if(str.charAt(str.length-1) === "/") { 
-                str = str.substr(0, str.length - 1);
-            }
-            return str;
-        }
+        return str;
     }
 
-    static Redirect(route){
-        if(route) {
-            window.location.hash = '#' + route;
-        } else {
-             window.location.hash = '#';
-        }
+    static Redirect(route = ''){
+        window.location.hash = `#${route}`;
     }
-
 }

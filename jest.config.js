3f82818df9f70e7eb32ef05f22a9f25dc011e674
AJ <82418212+AntonyAroni@users.noreply.github.com>
2023-12-26 18:27:54 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 // jest.config.js
 module.exports = {
-  roots: ['<rootDir>/tests/unit'],
-  testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+    //testMatch: ["<rootDir>/tests/unit/**/*.js"],
+    testMatch: ["<rootDir>\tests\unit\**\*.js"],
 };
-

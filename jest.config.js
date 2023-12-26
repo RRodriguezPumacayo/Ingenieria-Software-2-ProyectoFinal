a1372ec0b8825b1bd61badbe7be4fe08d7e480f7
AJ <82418212+AntonyAroni@users.noreply.github.com>
2023-12-26 18:37:38 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 // jest.config.js
 module.exports = {
-    //testMatch: ["<rootDir>/tests/unit/**/*.js"],
-    testMatch: ["<rootDir>\tests\unit\**\*.js"],
+    testMatch: ["<rootDir>/tests/unit/**/*.js"],
 };

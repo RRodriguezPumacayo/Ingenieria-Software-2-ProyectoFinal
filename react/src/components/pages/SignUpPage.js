868adb159d051813053d1fb07f17e874ca59dbbc
AntonyAroni <aaronij@unsa.edu.pe>
2023-12-26 13:31:23 -0500

    Solucionar code smells

--- react/src/components/pages/SignUpPage.js
+++ react/src/components/pages/SignUpPage.js
@@ -1,60 +1,50 @@
-import {react} from 'react';
-import $ from 'jquery';
+import React, { useRef } from 'react';
+import axios from 'axios';
 import config from 'Config';
 import Events from 'utilities/Events';
 import {Helper} from 'utilities/Helper';
 
-export default class SignUpPage extends React.Component {
-    constructor(){
-        super(...arguments);        
-    }
+export default function SignUpPage() {
+    const nameRef = useRef();
+    const passwordRef = useRef();
 
-    createUser(username, password) {
-        
-        let def = $.Deferred();
-
-        $.ajax({
-            type: "POST",
-            url: config.apiURL + "/user",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            data: JSON.stringify({username: username, password: password}),				
-            success:(data) => {
-                Events.broadcast('notification', 'User added', 'success');
-                def.resolve(data);
-            },
-            error:(errorMessage) => {
-
-                if(errorMessage.status === 403) {
-                    Events.broadcast('notification', 'That username already exists. Please choose a different username', 'error');
-                } else {
-                    Events.broadcast('notification', 'Error signing up', 'error');
-                }
-
-                def.reject();
+    const createUser = async (username, password) => {
+        try {
+            const response = await axios.post(config.apiURL + "/user", { username, password });
+            Events.broadcast('notification', 'User added', 'success');
+            return response.data;
+        } catch (error) {
+            if (error.response && error.response.status === 403) {
+                Events.broadcast('notification', 'That username already exists. Please choose a different username', 'error');
+            } else {
+                Events.broadcast('notification', 'Error signing up', 'error');
             }
-        }); 
-
-        return def.promise();       
+            throw error;
+        }
     }
 
-    signup(){
-        let username = ReactDOM.findDOMNode(this.refs.name).value
-        let password = ReactDOM.findDOMNode(this.refs.password).value
-        this.createUser(username, password).then((user) => { 
+    const signup = async () => {
+        const username = nameRef.current.value;
+        const password = passwordRef.current.value;
+        try {
+            await createUser(username, password);
             Helper.Redirect();
-        }).fail(() => {
+        } catch {
             Helper.Redirect('signup');
-        });
-    }    
-    render() {
-        return (
-            <div id="sign-up">
-                <h2>Sign Up</h2>
-                <p><label>User Name</label> <input name="name" type="text" ref="name" /></p>
-                <p><label>Password</label><input name="password" type="password" ref="password" /></p>
-                <button id="signup" onClick={() => this.signup()}>Sign Up</button>
-            </div>
-        );
+        }
     }
+
+    return (
+        <div id="sign-up">
+            <h2>Sign Up</h2>
+            <p>
+                <label>User Name<input name="name" type="text" ref={nameRef}/>
+                </label>
+            </p>
+            <p>
+                <label>Password<input name="password" type="password" ref={passwordRef}/></label>
+            </p>
+            <button id="signup" onClick={signup}>Sign Up</button>
+        </div>
+    );
 }

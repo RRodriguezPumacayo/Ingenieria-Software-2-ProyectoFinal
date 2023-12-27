5433e3f7b40b30ad65737297018ccbd1626c11dc
AntonyAroni <aaronij@unsa.edu.pe>
2023-12-26 19:24:11 -0500

    agregar configuracion

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 // jest.config.js
 module.exports = {
-    testMatch: ["C:\\Users\\anton\\.jenkins\\workspace\\test\\tests\\unit\\**\\*.js"],
+    testMatch: ["C:/Users/anton/.jenkins/workspace/test/tests/unit/**/*.js"],
 };
